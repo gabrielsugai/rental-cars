7942daa78a4e5ebf596c826ee5ebe398fd990033
Gabriel Sugai <gabrielsugai@gmail.com>
2020-04-16 23:57:32 -0300

    Admin pode editar fabricantes

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -20,6 +20,19 @@ class ManufacturersController < ApplicationController
     end
   end
 
+  def edit
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def update
+    @manufacturer = Manufacturer.find(params[:id])
+    if @manufacturer.update(manufacturer_params)
+      redirect_to @manufacturer
+    else
+      render :edit
+    end
+  end
+
   private
   
     def manufacturer_params
