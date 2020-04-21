93e841f375851e0a8a5b8760695bb38e754f6f30
Gabriel Sugai <gabrielsugai@gmail.com>
2020-04-21 02:27:23 -0300

    Finalizado pendencias da lista de car category

--- spec/features/admin_view_car_categories_spec.rb
+++ spec/features/admin_view_car_categories_spec.rb
@@ -32,4 +32,39 @@ feature 'Admin view car categories' do
     expect(page).not_to have_content('Seguro: R$ 60,00')
     expect(page).not_to have_content('Seguro de terceiros: R$ 70,00')
   end
+
+  scenario 'and no categories are created' do
+    
+    visit root_path
+    click_on 'Categorias de carros'
+
+    expect(page).to have_content('Nenhuma categoria cadastrada!')
+  end
+
+  scenario 'view categories and return to index' do
+
+    visit root_path
+    click_on 'Categorias de carros'
+    
+    expect(page).to have_content('Nenhuma categoria cadastrada!')
+
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'view details and return to categories' do
+    CarCategory.create!(name:'A', daily_rate: 80.0, car_insurance: 40.0, third_party_insurance: 50.0)
+    CarCategory.create!(name:'B', daily_rate: 100.0, car_insurance: 60.0, third_party_insurance: 70.0)
+    
+    visit root_path
+    click_on 'Categorias de carros'
+    click_on 'Categoria A'
+
+    expect(page).to have_content('Categoria: A')
+
+    click_on 'Voltar'
+
+    expect(current_path).to eq car_categories_path
+  end
 end
